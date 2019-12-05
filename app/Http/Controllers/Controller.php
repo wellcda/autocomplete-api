84acc026f0ce1f977abc9a3b0e2cb2f247450240
Wellington Aguiar <wellington@castrodeaguiar.com>
2019-12-05 12:33:53 -0300

    master | adding response methods to base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,9 +5,65 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @param string $message
+     * @param mixed  $data
+     *
+     * @return array
+     */
+    public static function createResponse($message, $data)
+    {
+        return [
+            'success' => true,
+            'data'    => $data,
+            'message' => $message,
+        ];
+    }
+
+    /**
+     * @param string $message
+     * @param array  $data
+     *
+     * @return array
+     */
+    public static function createError($message, $data = [])
+    {
+        $response = [
+            'success' => false,
+            'message' => $message,
+        ];
+
+        if (!empty($data)) {
+            $response['data'] = $data;
+        }
+
+        return $response;
+    }
+
+    public function sendResponse($result, $message = 'Action successful')
+    {
+        return Response::json($this->createResponse($message, $result));
+    }
+
+    public function sendError($message, $data = [], $code = 404)
+    {
+        return Response::json($this->createError($message, $data), $code);
+    }
+
+    public function validationError($message = 'Validation error', $data = [], $errorCode = 400)
+    {
+        return Response::json($this->createError($message, $data), $errorCode);
+    }
+
+    public function unauthorizedError()
+    {
+        return Response::json($this->createError('Unauthorized'), 401);
+    }
 }
